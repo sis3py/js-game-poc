7d5bbb354ca606ef3f1392be9ff2adb478686d76
sis3py <romainmx@gmail.com>
2018-10-13 00:37:19 +0200

    Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,69 @@
-const common = require('./webpack/webpack.common');
-const webpackMerge = require('webpack-merge');
+'use strict';
 
-const envs = {
-  development: 'dev',
-  production: 'prod',
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.jsx',
+
+    output: {
+        // change this to `bundle.js` if you want it to be outputted to the same folder as HTML
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.vert$/, /\.frag$/],
+                exclude: /node_modules/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './assets'
+                }
+            },
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        }),
+        new HtmlWebPackPlugin({
+            filename: path.resolve(__dirname, 'dist/index.html'),
+            template: path.resolve(__dirname, 'src/template/template.html'),
+            inject: true
+        }),
+        new CopyWebpackPlugin([
+            { from: 'src/client/assets', to: 'assets' }
+        ]),
+    ]
 };
-/* eslint-disable global-require,import/no-dynamic-require */
-const env = envs[process.env.NODE_ENV || 'development'];
-const envConfig = require(`./webpack/webpack.${env}.js`);
-module.exports = webpackMerge(common, envConfig);
